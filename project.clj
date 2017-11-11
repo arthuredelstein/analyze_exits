898f491ddf34a19bd3394755a70723c8ae19cdf3
Arthur Edelstein <arthuredelstein@gmail.com>
2017-11-10 19:07:28 -0500

    first commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject analyze_exits "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+                 [hiccup "1.0.5"]
+                 [org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 ])
---
+++ src/analyze_exits/core.clj
@@ -0,0 +1,125 @@
+(ns analyze-exits.core
+  (require
+   [clojure.data.json :as json]
+   [clojure.java.io :as io]
+   [clojure.string :as s]
+   [hiccup.page :as page]
+   [hiccup.util :as html-util]
+   ))
+
+(defn date-from-filename
+  [filename]
+  (let [text (re-find #"[0-9]+_[0-9]+" filename)
+        formatter (java.text.SimpleDateFormat. "yyyyMMdd_hhmm")]
+    (.parse formatter text)))
+
+(defn utc-string
+  [date]
+  (let [tz (java.util.TimeZone/getTimeZone "UTC")
+        df (java.text.SimpleDateFormat. "yyyy-MM-dd HH:mm 'UTC'")]
+    (.setTimeZone df tz)
+    (.format df date)))
+
+
+(defn raw-results-files []
+  (->> (file-seq (io/as-file "../all_exit_results"))
+       (map #(.getPath %))
+       (filter #(.endsWith % ".json"))
+       sort))
+
+(defn read-result-file
+  "Load the data from a result file."
+  [file]
+  (json/read-str (slurp file)))
+
+(defn timeout-rates [result-data]
+  (into {}
+        (for [[fp results] (result-data "example.com")]
+          [fp
+           (double (/ (count (filter #(= (% 0) "TIMEOUT")
+                                     results))
+                      (count results)))])))
+
+(def fields
+  ["nickname"
+   "fingerprint"
+   "as_name"
+   "country_name"
+   "contact"
+   "platform"
+;   "or_addresses"
+   "bandwidth_rate"
+   "consensus_weight"
+   ])
+
+(defn onionoo-exits
+  [fields]
+  (let [url (str "https://onionoo.torproject.org/details?type=relay&flag=exit&fields="
+                 (s/join "," fields))]
+    (-> url
+        read-result-file
+        (get "relays"))))
+
+(defn as-percent
+  [val]
+  (format "%3.1f" (* 100. val)))
+
+(defn assemble-data-table
+  [exits fields timeout-rates]
+  (let [exits-map (group-by #(get % "fingerprint") exits)]
+    (for [[fp timeout-rate] timeout-rates]
+      (when-let [exit (first (exits-map fp))]
+        (let [timeout-percent (as-percent timeout-rate)]
+          (cons timeout-percent (map exit fields)))))))
+
+(defn mean
+  [numbers]
+  (/ (apply + numbers) (count numbers)))
+
+(defn data-table
+  []
+  (let [fields+ (cons "dns_timeout_percent" fields)
+        latest-result-file (last (raw-results-files))
+        latest-result (read-result-file latest-result-file)
+        file-date (date-from-filename latest-result-file)
+        timeout-rates (timeout-rates latest-result)
+        exits (onionoo-exits fields)
+        average (mean (vals timeout-rates))
+        raw-data-table (assemble-data-table exits fields timeout-rates)]
+    [fields+ (reverse (sort-by #(Double/parseDouble (first %)) (remove nil? raw-data-table))) average file-date]))
+
+(defn html-table
+  [header body]
+  [:table
+   [:tr (for [item header]
+          [:th item])]
+   (for [row body]
+     [:tr (for [item row]
+            [:td item])])])
+
+(defn html-page
+  [header body average file-date]
+  (page/html5
+   [:head
+    [:title "Tor Exit DNS Timeouts"]
+    [:meta {:charset "utf-8"}]
+    [:style {:type "text/css"} (slurp "main.css")]
+    ]
+   [:body
+    [:h2 "Tor Exit DNS Timeouts"]
+    [:p (str "Average unweighted timeout rate: " (as-percent average) "%")]
+    [:p (str "Test ran at " (utc-string file-date))]
+    (html-table header body)
+    ]))
+
+(defn write-page!
+  [filename]
+  (let [[header body average file-date] (data-table)]
+    (spit (str "../exits/" (or filename "temp.html"))
+          (html-page header body average file-date))))
+
+(defn -main
+  "Main program."
+  []
+  (write-page! "index.html")
+  )
