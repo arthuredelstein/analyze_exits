c9d8deda118f0e6864e64402107cd3171596c3f7
Arthur Edelstein <arthuredelstein@gmail.com>
2019-06-02 19:10:57 -0400

    exits + relays

--- src/analyze_exits/core.clj
+++ src/analyze_exits/core.clj
@@ -25,8 +25,14 @@
     (.format df date)))
 
 
-(defn raw-results-files []
-  (->> (file-seq (io/as-file "../all_exit_results"))
+(defn mean
+  [numbers]
+  (/ (apply + numbers) (count numbers)))
+
+(defn raw-results-files [type]
+  (->> (file-seq (io/as-file (if (= type :exits)
+                               "../all_exit_results"
+                               "../all_relay_results")))
        (map #(.getPath %))
        (filter #(.endsWith % ".json"))
        (remove #(.endsWith % "_latest.json"))
@@ -37,14 +43,26 @@
   [file]
   (json/read-str (slurp file)))
 
-(defn timeout-rates [result-data]
+(defn failure-rates
+  [result-data]
   (into {}
         (for [[fp results] (result-data "example.com")]
-          (let [timeout-count (count (filter #(= (% 0) "TIMEOUT") results))
-                success-count (count (filter #(= (% 0) "SUCCEEDED") results))
-                total-count (+ success-count timeout-count)]
+          (let [fp1 (.replace fp "$" "")
+                total-count (count results)
+                failure-count (count (filter #(not= (% 0) "SUCCEEDED") results))]
             (when (> total-count 0)
-              [fp (double (/ timeout-count total-count))])))))
+              [fp1 (double (/ failure-count total-count))])))))
+
+(defn connection-times
+  [result-data]
+  (into {}
+        (for [[fp results] (result-data "example.com")]
+          (let [fp1 (.replace fp "$" "")
+                times (->> results
+                           (map #(nth % 2))
+                           (filter #(not= -1 %)))]
+            (when (pos? (count times))
+              [fp1 (mean times)])))))
 
 (def fields
   ["nickname"
@@ -60,7 +78,7 @@
 
 (defn onionoo-exits
   [fields]
-  (let [url (str "https://onionoo.torproject.org/details?type=relay&flag=exit&fields="
+  (let [url (str "https://onionoo.torproject.org/details?type=relay&fields="
                  (s/join "," fields))]
     (-> url
         read-result-file
@@ -70,47 +88,54 @@
   [val]
   (format "%3.1f" (* 100. val)))
 
-(defn average-timeout-rate [exits timeout-rates]
+(defn average-failure-rate [exits failure-rates]
   (apply +
          (for [exit exits]
            (* (get exit "exit_probability" 0)
               (or
-               (get timeout-rates (get exit "fingerprint"))
+               (get failure-rates (get exit "fingerprint"))
                0)))))
 
 (defn assemble-data-table
-  [exits fields timeout-rates]
+  [exits fields failure-rates connection-times]
   (let [exits-map (group-by #(get % "fingerprint") exits)]
-    (for [[fp timeout-rate] timeout-rates]
-      (when-let [exit (first (exits-map fp))]
-        (let [timeout-percent (as-percent timeout-rate)]
-          (-> exit
-              (assoc "dns_timeout_percent" timeout-percent)))))))
-
-(defn mean
-  [numbers]
-  (/ (apply + numbers) (count numbers)))
+    (for [[fp failure-rate] failure-rates]
+      (let [connection-time (connection-times fp)]
+        (when-let [exit (first (exits-map fp))]
+          (let [failure-percent (as-percent failure-rate)]
+            (-> exit
+                (assoc "failure_rate" failure-percent)
+                (assoc "connection_time" connection-time))))))))
 
 (defn timeout-probability
   [data-row]
   (let [exit-probability (or (get data-row "exit_probability") 0.000001)
-        timeout-rate (Double/parseDouble (get data-row "dns_timeout_percent"))]
-    (* exit-probability timeout-rate)))
+        failure-rate (Double/parseDouble (get data-row "failure_rate"))]
+    (* exit-probability failure-rate)))
 
 (defn data-table
-  []
-  (let [fields+ (concat ["dns_timeout_percent"] fields)
-        latest-result-file (last (raw-results-files))
+  [type]
+  (let [fields+ (concat ["failure_rate" "connection_time"] fields)
+        latest-result-file (last (raw-results-files type))
+;        _ (println latest-result-file)
         latest-result (read-result-file latest-result-file)
+;        _ (println latest-result)
         exits (or (latest-result "_relays")
-                  (comment (onionoo-exits fields)))
+                  (onionoo-exits fields))
+;        _ (println exits)
         file-date (date-from-filename latest-result-file)
-        timeout-rates (timeout-rates latest-result)
-        average (average-timeout-rate exits timeout-rates)
-        raw-data-table (assemble-data-table exits fields timeout-rates)
+        failure-rates (failure-rates latest-result)
+        connection-times (connection-times latest-result)
+;        _ (println failure-rates)
+        average (average-failure-rate exits failure-rates)
+;        _ (println average)
+        raw-data-table (assemble-data-table exits fields
+                                            failure-rates connection-times)
+;        _ (println raw-data-table)
         body (->> raw-data-table
                   (remove nil?)
-                  (sort-by timeout-probability)
+                  (sort-by #(+ (get % "connection_time")))
+                  (sort-by (if (= type :exits) timeout-probability #(Double/parseDouble (get % "failure_rate"))))
                   reverse)]
     [fields+ body average file-date]))
 
@@ -127,59 +152,63 @@
                 [:td text])))])])
 
 (defn html-page
-  [header body average file-date]
+  [type title header body average file-date]
   (page/html5
    [:head
-    [:title "Tor Exit DNS Timeouts"]
+    [:title title]
     [:meta {:charset "utf-8"}]
     [:style {:type "text/css"} (compress-css (slurp "main.css"))]
+    [:link {:href "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=" :rel "icon" :type "image/x-icon"}]
     ]
    [:body
-    [:h2 "Tor Exit DNS Timeouts"]
+    [:h2 title]
     [:div {:style "max-width: 600px"}
-     [:p (str "Average probability-weighted timeout rate: " (as-percent average) "%")]
+     [:p (str "Average probability-weighted failure rate: " (as-percent average) "%")]
      [:p (str "Test ran at " (utc-string file-date))]
      [:p {:style "font-weight: bold; color: DarkRed;"}
-      "Dear exit relay operator: is your exit timing out on DNS requests? Please fix the problem by "
-      [:a {:href "https://lists.torproject.org/pipermail/tor-announce/2017-December/000147.html"}
-       "upgrading your tor relay"] "."]
+      "Dear exit relay operator: are connections to your exit failing or slow? Please check for connectivity, latency and DNS timeouts."]
      [:p "Source code: "
       [:a {:href "https://github.com/arthuredelstein/tor_dns_survey"} "scanner"]
       " | "
       [:a {:href "https://github.com/arthuredelstein/analyze_exits"} "table"]]
      [:p "Historical JSON data is "
-      [:a {:href "/exits-json/"} "here"] ". "
+      [:a {:href (if (= type :exits) "/exits-json/" "/relays-json/")} "here"] ". "
       "Latest JSON results are "
-      [:a {:href "/exits-json/exit_results_latest.json"} "here"] "."]]
+      [:a {:href (if (= type :exits) "/exits-json/exit_results_latest.json" "/relays-json/relay_results_latest.json")} "here"] "."]]
     (html-table header body)
     ]))
 
 (defn write-page!
-  [filename]
-  (let [[header body average file-date] (data-table)]
-    (spit (str "../exits/" (or filename "temp.html"))
-          (html-page header body average file-date))))
+  [type dir filename]
+  (let [[header body average file-date] (data-table type)]
+    (spit (str dir (or filename "temp.html"))
+          (html-page type
+                     (if (= type :exits)
+                       "Tor Exit Failures" "Tor Relay Failures")
+                     header body average file-date))))
 
 (defn daily-raw-to-summary! [file]
   (let [data (read-result-file file)
         name (.getName (io/as-file file))
-        rates (timeout-rates data)]
+        rates (failure-rates data)]
     (when (not-empty rates)
-      (let [rates (timeout-rates data)]
+      (let [rates (failure-rates data)]
         (spit (str "../json/" name)
               (json/write-str rates))
-        (spit (str "../json/exit_results_latest.json")
+        (spit (str "../json/" (if (= type :exits) "exit" "relay")
+                                  "_results_latest.json")
               (json/write-str rates))))
     name))
 
 (defn -main
   "Main program."
   []
-  (write-page! "index.html")
-  (daily-raw-to-summary! (last (raw-results-files)))
+  (write-page! :exits "../exits/" "index.html")
+  (daily-raw-to-summary! (last (raw-results-files :exits)))
+  (write-page! :relays "../relays/" "index.html")
+  (daily-raw-to-summary! (last (raw-results-files :relays)))
   )
 
 (defn generate-all-daily-summaries! []
-  (doseq [file (raw-results-files)]
+  (doseq [file (raw-results-files :exits)]
     (println (daily-raw-to-summary! file))))
-
